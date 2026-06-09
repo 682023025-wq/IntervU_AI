7531e19d4ac597791d3f54a777ba61d17b9ee700
682023025-wq <email@682023025-wq.github.com>
2026-06-09 19:42:14 +0700

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Izinkan akses dari jaringan eksternal
+    host: '0.0.0.0', // Izinkan akses dari jaringan luar
     port: 5173,
     strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: '192.168.0.65', // Ganti dengan IP lokal Anda saat ini, atau gunakan '0.0.0.0' jika dinamis
+      port: 5173,
+      clientPort: 5173,
+    },
   },
 })
